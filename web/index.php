c48630fd867c211a324ba353e94d339b9db2ef75
bennyboulter <bennyboulter@gmail.com>
2014-05-16 00:45:46 +0900

    Added Google Analytics code and doctype

--- web/index.php
+++ web/index.php
@@ -1,14 +1,24 @@
+<!DOCTYPE html>
 <html>
   <head>
     <title>Resumelon</title>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-36221015-2', 'resumelon.com');
+  ga('send', 'pageview');
+
+    </script>
     <meta property="og:title" content="Resumelon - A platform for making awesome resumes" />
-<meta property="og:type" content="company" />
-<meta property="og:site_name" content="Resumelon - A platform for making awesome resumes" />
-<meta property="og:description" content="<html><br><p>
-We are currently in the process of creating a method to cut back on the ridiculous amount of time we spend maintaining our own resumes. In doing so we hope to share with the world the awesome layout and personal branding standard we have come to appreciate. Leave your details below and we will be in touch! 
+    <meta property="og:type" content="company" />
+    <meta property="og:site_name" content="Resumelon - A platform for making awesome resumes" />
+    <meta property="og:description" content="<html>We are currently in the process of creating a method to cut back on the ridiculous amount of time we spend maintaining our own resumes. In doing so we hope to share with the world the awesome layout and personal branding standard we have come to appreciate. Leave your details below and we will be in touch! 
 </p><br><br></html>" />
-<meta property="og:url" content="www.resumelon.com" />
-<meta property="og:image" content="https://launchrock-assets.s3.amazonaws.com/facebook-files/MFXJQL6P_1399613725330.png?_=0" />
+    <meta property="og:url" content="www.resumelon.com" />
+    <meta property="og:image" content="https://launchrock-assets.s3.amazonaws.com/facebook-files/MFXJQL6P_1399613725330.png?_=0" />
   </head>
 
   <body>
