2f970f190b63c1abed3c44fa2e8c5b75da134ae6
Desi Cochrane <desi@desicochrane.com>
2014-05-15 20:58:41 +0900

    Added Launchrocket Widget

--- web/index.php
+++ web/index.php
@@ -1,1 +1,13 @@
-<html><body>Test!</body></html>
+<html>
+  <head>
+    <title>Resumelon.com</title>
+  </head>
+
+  <body>
+    <!-- Begin LaunchRock Widget -->
+    <div id="lr-widget" rel="MFXJQL6P"></div>
+    <script type="text/javascript" src="//ignition.launchrock.com/ignition-current.min.js"></script>
+    <!-- End LaunchRock Widget -->
+  </body>
+
+</html>
