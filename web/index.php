ed143245d42b4dd744de08f034fb3eb19020b27a
bennyboulter <bennyboulter@gmail.com>
2014-05-16 00:01:06 +0900

    Added FB script from LaunchRock to header section

--- web/index.php
+++ web/index.php
@@ -1,10 +1,17 @@
 <html>
   <head>
-    <title>Resumelon.com</title>
+    <title>Resumelon</title>
+    <meta property="og:title" content="Resumelon - A platform for making awesome resumes" />
+<meta property="og:type" content="company" />
+<meta property="og:site_name" content="Resumelon - A platform for making awesome resumes" />
+<meta property="og:description" content="<html><br><p>
+We are currently in the process of creating a method to cut back on the ridiculous amount of time we spend maintaining our own resumes. In doing so we hope to share with the world the awesome layout and personal branding standard we have come to appreciate. Leave your details below and we will be in touch! 
+</p><br><br></html>" />
+<meta property="og:url" content="www.resumelon.com" />
+<meta property="og:image" content="https://launchrock-assets.s3.amazonaws.com/facebook-files/MFXJQL6P_1399613725330.png?_=0" />
   </head>
 
   <body>
-    neverevermouse
     <!-- Begin LaunchRock Widget -->
     <div id="lr-widget" rel="MFXJQL6P"></div>
     <script type="text/javascript" src="//ignition.launchrock.com/ignition-current.min.js"></script>
